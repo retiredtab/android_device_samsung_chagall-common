d739755af2939c5834c0655267c8b8b35ddf8c86
garak <elim.garak.dev@gmail.com>
2022-02-06 14:14:22 +0100

    chagall: fix for killing bootanimation
    For some reason lowmemorykiller kill bootanimation at the of animation and then the device restarts. To avoid this we reduced resolution for bootanimation.
    
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104] ===== DUMPING ION HEAPS =====
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104] [Name] ion_noncontig_heap, [ID] : 0
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104]           total         119427072
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104] ----------------------------------------------------
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104] [Name] exynos_noncontig_heap, [ID] : 5
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104]           total                 0
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104] ----------------------------------------------------
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104] [Name] exynos_contig_heap, [ID] : 4
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104]           total              4096
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104] ----------------------------------------------------
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104] [Name] ion_chunk_heap, [ID] : 6
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104]           total                 0
    12-18 21:53:08.305     0     0 I         : [B1:   BootAnimation:  3104] ----------------------------------------------------
    12-18 21:53:08.305     0     0 W [B1:   BootAnimation:  3104] lowmemorykiller: BootAnimation invoked Android-oom-killer: oom_adj=-17, oom_score_adj=-1000
    12-18 21:53:08.305     0     0 W [B1:   BootAnimation:  3104] Backtrace:
    ......12-18 21:53:08.329     0     0 W [B1:   BootAnimation:  3104] active_anon: 34132 inactive_anon:15260 isolated_anon:0
    12-18 21:53:08.329     0     0 W [B1:   BootAnimation:  3104]  active_file: 28541 inactive_file:54186 isolated_file:0
    12-18 21:53:08.329     0     0 W [B1:   BootAnimation:  3104]  unevictable: 0 dirty:18 writeback:0 unstable:0
    12-18 21:53:08.329     0     0 W [B1:   BootAnimation:  3104]  free: 434128 slab_reclaimable:2252 slab_unreclaimable:5109
    12-18 21:53:08.329     0     0 W [B1:   BootAnimation:  3104]  mapped: 30364 shmem:601 pagetables:1988 bounce:0
    12-18 21:53:08.329     0     0 W         : [B1:   BootAnimation:  3104] Normal free:2908kB min:2836kB low:3544kB high:4252kB active_anon:0kB inactive_anon:0kB active_file:68kB inactive_file:220kB unevictable:0kB isolated(anon):0kB isolated(file):0kB present:503440kB mlocked:0kB dirty:0kB writeback:0kB mapped:0kB shmem:0kB slab_reclaimable:9008kB slab_unreclaimable:20436kB kernel_stack:3304kB pagetables:7952kB unstable:0kB bounce:0kB writeback_tmp:0kB pages_scanned:0 all_unreclaimable? no
    12-18 21:53:08.329     0     0 W [B1:   BootAnimation:  3104] lowmem_reserve[]: 0 5094 5094
    12-18 21:53:08.329     0     0 W         : [B1:   BootAnimation:  3104] HighMem free:1733604kB min:512kB low:4184kB high:7856kB active_anon:136528kB inactive_anon:61040kB active_file:114096kB inactive_file:216524kB unevictable:0kB isolated(anon):0kB isolated(file):0kB present:2608548kB mlocked:0kB dirty:72kB writeback:0kB mapped:121456kB shmem:2404kB slab_reclaimable:0kB slab_unreclaimable:0kB kernel_stack:0kB pagetables:0kB unstable:0kB bounce:0kB writeback_tmp:0kB pages_scanned:0 all_unreclaimable? no
    12-18 21:53:08.329     0     0 W [B1:   BootAnimation:  3104] lowmem_reserve[]: 0 0 0

--- device-common.mk
+++ device-common.mk
@@ -36,8 +36,8 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/bluetooth/bt_vendor.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/bluetooth/bt_vendor.conf
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2560
-TARGET_SCREEN_WIDTH := 1600
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
 
 # Fingerprint
 PRODUCT_PACKAGES += \
